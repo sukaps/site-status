0db8a103c490eba66f8b65f02652c3314b8ee76a
Sukap <xsukap@gmail.com>
2023-12-13 22:57:02 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -22,21 +22,14 @@ const Footer = () => {
       </div>
       <div className="text">
         <p>
-          <CustomLink
-            text={Package.alia}
-            to="https://github.com/imsyy/site-status"
-          />
-          &nbsp;Version&nbsp;{Package.version}
-        </p>
-        <p>
           基于&nbsp;
           <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
           &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
         </p>
         <p>
-          Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
+          Copyright&nbsp;&copy;&nbsp;2018&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://sukap.cn/" text="Sukap" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
